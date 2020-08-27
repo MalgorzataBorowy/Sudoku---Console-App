4a1413227ff8c6e7064ba9dc44e3fc4427462a51
MalgorzataBorowy <borowy.malgorzata@gmail.com>
2020-08-27 23:45:55 +0200

    Initial commit

---
+++ SudokuApp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace SudokuApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            SudokuGame game1 = new SudokuGame();
+            game1.generateSudoku();
+            game1.printSudoku();
+        }
+    }
+}
---
+++ SudokuApp/SudokuGame.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SudokuApp
+{
+    class SudokuGame
+    {
+        private const int size = 9;
+        int[,] matrix = new int[size,size];
+
+        private int generateRandom()
+        {
+            Random rnd = new Random();
+            return rnd.Next(1,10);
+        }
+
+        private bool checkSquare(int value, int k, int r)
+        {
+            for(int i=k; i<k+3; i++)
+            {
+                for(int j=r; j<r+3; j++)
+                {
+                    if (matrix[i, j] == value)
+                        return false;
+                }
+            }
+            return true;
+        }
+
+        private bool checkRow(int value, int r)
+        {
+            for (int i = 0; i < size; i++)
+            {
+                if (matrix[r, i] == value)
+                    return false;               
+            }
+            return true;
+        }
+        private bool checkColumn(int value, int k)
+        {
+            for (int i = 0; i < size; i++)
+            {
+                if (matrix[i, k] == value)
+                    return false;
+            }
+            return true;
+        }
+
+        private bool fillSquare(int k, int r)
+        {        
+            for (int i = k-1; i<k+2; i++)
+            {                
+                for(int j=r-1; j<r+2; j++)
+                {
+                    List<int> table = new List<int>();
+                    //int count = 0;
+                    int rnd = generateRandom();
+                    while( !( checkSquare(rnd,k-1,r-1) && (checkRow(rnd,i)) && checkColumn(rnd,j) ) )
+                    {
+                        rnd = generateRandom();
+                        //count++;
+                        if(!table.Contains(rnd))
+                        {
+                            table.Add(rnd);
+                        }
+                        if (table.Count >= 9)
+                            return false;
+                    }
+                    matrix[i, j] = rnd;
+                    /*foreach(int item in table)
+                        Console.Write($"{item} ");
+                    printSudoku();*/
+                }
+            }
+            return true;
+        }
+
+        private void fillWithZeros()
+        {
+            for(int i=0;i<size;i++)
+            {
+                for (int j = 0; j < size; j++)
+                    matrix[i, j] = 0;
+            }
+        }
+
+        private void generateDiagonal()
+        {
+            fillSquare(1, 1);
+            fillSquare(4, 4);
+            fillSquare(7, 7);
+        }
+        private bool generateNonDiagonal()
+        {
+            int cos = 0;
+            generateDiagonal();
+            for(int i=1; i<size; i+=3)
+            {
+                for(int j=1; j<size; j+=3)
+                {
+                    if (i != j)
+                    {
+                        bool flag = fillSquare(i, j);
+                        if (!flag)
+                        {
+                            return false;
+                        }
+                        cos++;
+                        if (cos == 6)
+                            return true;
+                    }  
+                }
+            }
+            return true;
+        }
+
+        public void generateSudoku()
+        {
+            bool flag = generateNonDiagonal();
+            while(!flag)
+            {
+                fillWithZeros();
+                flag = generateNonDiagonal();
+                printSudoku();
+            }
+        }
+
+        public void printSudoku()
+        {
+
+            for(int i=0; i<size+1; i++)
+            {
+                if((i+3)%3==0)
+                {
+                    for (int n = 0; n < 25; n++)
+                        Console.Write("--  ");
+                    Console.Write("\n\n");
+                }  
+                if (i<size)
+                {
+                    for(int j=0; j<size; j++)
+                    {
+                        if ((j+3) % 3 == 0)
+                            Console.Write("|\t");
+                        Console.Write($"{matrix[i,j]}\t");
+                    }
+                    Console.WriteLine("|\n");
+                }                  
+            }
+        }
+
+    }
+}
